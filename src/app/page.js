5ab31afcfde1fb43102da882416e083c7621690d
Joshua Lee <joshrhily@Joshuas-MacBook-Air.local>
2024-02-12 15:49:39 -0800

    test commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ const leage = League_Spartan({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className={leage.className}>
+      <h1>Hello, World!</h1>
       <TitleBox name="Joshua Lee" title="Software/Web Developer" desc="I develop apps/web apps to help with everyday life"/>
     </div>
   );
